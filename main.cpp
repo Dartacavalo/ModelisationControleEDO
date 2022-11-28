4d300e073a325e0f18a0cf751e79db3f3b1365ac
themathsdude <elyass2008@hotmail.fr>
2022-11-28 18:26:17 +0100

    change directory

--- main.cpp
+++ main.cpp
@@ -1,61 +1,92 @@
 //
 //  main.cpp
-//  POO_EDO
+//  classEDO
 //
-//  Created by José Maria Marques on 07/11/2022.
+//  Created by José Maria Marques on 14/11/2022.
 //
 
+
+
+#include "PbCauchy.hpp"
+#include "Solver.hpp"
+#include "CasTest.hpp"
+
+#include <math.h>
 #include <iostream>
-#include <cmath>
+#include <vector>
 
-class CI{
-    public:
-    
-    //Le domaine
-    double const debut;
-    double const fin;
-    //Les conditions initiales
-    double const y0;
-};
+using namespace std;
 
-class Pb_Cauchy{
-    public:
-    //conditions initiales
-    CI cond_init;
-    //Fonction
-    double F(double x, double f);
-};
+// fonction de 1er essay
+double essay1(double t, double x){
+    return -2*t*t;
+}
 
+double exact(double t){
+    return exp(-t*t);
+}
 
-class Tests{
-};
+// fonctioon de 2em3 essay
+double essay2(double t, double x){
+    return 1;
+}
 
-
-class Refroidissement: public Tests{
-    public:
-    double k;
-    double T0;
-    Refroidissement(double _k, double _T0, double t): k(_k), T0(_T0){};
-    
-    double f(double t){return -k * (t - T0);};
-    
-};
-
-
-class Schemas{
-    public:
-};
-
-class noclass {
-    public:
-        double wesh;
-        double weshwesh;
-};
-
-//
+double exact1(double t){
+    return t;
+}
 
 
 int main(){
-    return 0;
-}
+    
+    double a = 0;
+	double b = 10;
+    double x0 = exact(a);
+    int N = 1000;
+    
+    PbCauchy test = PbCauchy(x0, essay1);
+    
+    EulerExplicite schema(a, b, N, "schema_EDO_EuExp", test);
+    schema.calcul();
+	
+    CasTest castest = CasTest(exact, essay1, a, b, 10, 1000, 10, "schema_test_ptr_exp_RK", "RK");
+	castest.error_export();
+	
+	
+//	Solver* eul = new EulerExplicite(a, b, N, "schema_EDO_EuExp", test);
+//	vector<double> sol_approch = eul->get_x_val();
+//	vector<double> t = eul->get_t_val();
+//	vector<double> sol_exacte = eul->get_x_val();
+//	for(double i = 0; i<1000; i++){
+//		cout<<t[i]<<"  "<<sol_approch[i]<<"  "<<sol_exacte[i]<<"  "<<sol_approch[i] - exact(t[i]) << endl;
+	}
+	
+//	delete eul;
+	
+//	castest.maj_schema(100);
+//	castest.calcul();
+//	castest.exacte(100);
+//    castest.error_export();
 
+//    RungeKutta schema_RK(a, b, N, "schema_EDO_RK", test);
+//    schema_RK.expor();
+//
+//    RungeKutta schema1_RK = RungeKutta(a, b, N, "schema_EDO_RK", test);
+//    CasTest castest_RK(exact, essay1, schema1_RK, 100, 10000, 100);
+//    castest_RK.exact_export();
+
+//	cout<<castest.calcul_pente()<<endl;
+//	cout<<castest_RK.calcul_pente()<<endl;
+////    castest_RK.error_export();
+//
+//    EulerExplicite schema2 = EulerExplicite(a, b, 100, test);
+//    CasTest castest1(exact, essay1, schema2, 1000, 10000, 100);
+//    castest.exact_export();
+//    castest.error_export();
+//
+//    return 0;
+//}
+// Séance 13 Novembre:
+/*prochaine fois : gnuplot, cas test pour le schema: constante et expo,
+classe: cas test: PbCauchy et Sol exacte ou fichier test.cpp avec erreur et ordre de convergence du schema et representer l'erreur en fonction de h
+ autres types de schemas
+*/
