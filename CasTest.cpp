ed0d6a4041a0007438b7c9169f67fd6a4ddcae19
themathsdude <elyass2008@hotmail.fr>
2022-11-29 00:49:54 +0100

    refactoring changes and structure

--- CasTest.cpp
+++ CasTest.cpp
@@ -1,100 +1,106 @@
-//
-//  CasTest.cpp
-//  ProjetC++
-//
-//  Created by José Maria Marques on 20/11/2022.
-//
-
-
-
+#include "CasTest.hpp"
 #include "PbCauchy.hpp"
 #include "Solver.hpp"
-#include "CasTest.hpp"
 
+#include <functional>
 #include <iostream>
 #include <math.h>
-#include <vector>
-#include <functional>
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 
-//Solver _schema,
-//Solver(double _a, double _b, int _N, PbCauchy _EDO);
-
-CasTest::CasTest(function<double(double)> _fct_sol_exacte, function<double(double, double)> _fct_second_membre, double const _a, double const _b, double const _N_min, double const _N_max, double const _pas, string _nom_schema, string _type_schema): fct_sol_exacte(_fct_sol_exacte), fct_second_membre(_fct_second_membre), a(_a), b(_b),  N_min(_N_min), N_max(_N_max), pas(_pas), nom_schema(_nom_schema), type_schema(_type_schema){}
-
-Solver* CasTest::def_schema(double n){
-	PbCauchy pbcauchy(fct_sol_exacte(a), fct_second_membre);
-	if(type_schema == "EuExp"){
-		EulerExplicite* schema = new EulerExplicite(a, b, n, nom_schema, pbcauchy);
-		return schema;
-	}
-	else /*if (type_schema == "RK")*/{
-		RungeKutta* schema = new RungeKutta(a, b, n, nom_schema, pbcauchy);
-		return schema;
-	}
-}
-
-//void CasTest::set_tps(Solver* schema){
-//	tps = schema->t_val;
-//}
-//
-//void CasTest::set_sol_approch(Solver* schema){
-//	sol_approch = schema->x_val;
-//}
-
-void CasTest::set_sol_exacte(double n){
-	for(double i=0; i<= n; i++){
-		sol_exacte.push_back(fct_sol_exacte(tps[i]));
-	}
-}
-
-void CasTest::calcul_d_une_erreur(double n){
-	double err = 0;
-	Solver* schema = def_schema(n);
-//	tps.empty();
-//	sol_approch.empty();
-//	sol_exacte.empty();
-	tps = schema->t_val;
-	sol_approch = schema->x_val;
-	set_sol_exacte(n);
-	h.push_back(schema->dt);
-	for(double i = 0; i<=n; i++){
-//		cout<<sol_approch[i]<<endl;
-		err += (sol_exacte[i]-sol_approch[i])*(sol_exacte[i]-sol_approch[i]);
-			
-	}
-//		cout<<err<<endl;
-	
-	error_tot.push_back(sqrt(err));
-	delete schema;
-}
-
-void CasTest::calcul_erreur_totale(){
-	for(double n = N_min+1; n< N_max; n+= pas){
-		calcul_d_une_erreur(n);
-	}
-}
-
-double CasTest::calcul_pente(){
-	return (log(error_tot[97])-log(error_tot[0]))/(log(h[97])-log(h[0]));
-}
-
-void CasTest::error_export(){
-    ofstream erreur_schema;
-	string nom_erreur = nom_schema + "_erreur_schema.txt";
-    erreur_schema.open(nom_erreur);
-	calcul_erreur_totale();
-	cout<<calcul_pente()<<endl;
-    for(double i= 2; i < (N_max-N_min)/(int)pas; i++){
-        erreur_schema << h[i] <<" "<<error_tot[i]<<" "<<log(error_tot[i])<<endl;
+Solver *CasTest::def_schema(unsigned long long n)
+{
+    if (type_schema == "EuExp")
+    {
+        EulerExplicite *schema = new EulerExplicite(a, b, n, nom_schema, pbcauchy);
+        return schema;
     }
-    erreur_schema.close();
+    else /*if (type_schema == "RK")*/
+    {
+        RungeKutta *schema = new RungeKutta(a, b, n, nom_schema, pbcauchy);
+        return schema;
+    }
 }
 
+void CasTest::set_sol_exacte(Solver *schema, unsigned long long n)
+{
+    sol_exacte.clear();
+    for (unsigned long long i = 0; i <= n; i++)
+    {
+        sol_exacte.push_back(fct_sol_exacte(schema->t_val[i]));
+    }
+}
 
-//void CasTest::exact_export(double n){
+void CasTest::calcul_erreur_L2(unsigned long long n)
+{
+    double err = 0;
+    Solver *schema = def_schema(n);
+    set_sol_exacte(schema, n);
+    h.push_back(schema->dt);
+    for (double i = 0; i <= n; i++)
+    {
+        err += (sol_exacte[i] - schema->x_val[i]) * (sol_exacte[i] - schema->x_val[i]);
+    }
+
+    erreur_L2.push_back(sqrt(err));
+    delete schema;
+}
+
+void CasTest::calcul_erreur_max(unsigned long long n)
+{
+    double err = 0;
+    Solver *schema = def_schema(n);
+    set_sol_exacte(schema, n);
+    h.push_back(schema->dt);
+    for (double i = 0; i <= n; i++)
+    {
+        err = max(err, abs(sol_exacte[i] - schema->x_val[i]));
+    }
+
+    erreur_max.push_back(err);
+    delete schema;
+}
+
+void CasTest::calcul_erreur_totale()
+{
+    for (double n = N_min_erreurs + 1; n < N_max_erreurs; n += pas_erreurs)
+    {
+        calcul_erreur_L2(n);
+		calcul_erreur_max(n);
+    }
+}
+
+double CasTest::calcul_pente_max()
+{
+    return (log(erreur_max[97]) - log(erreur_max[0])) / (log(h[97]) - log(h[0]));
+}
+
+double CasTest::calcul_pente_L2()
+{
+    return (log(erreur_L2[97]) - log(erreur_L2[0])) / (log(h[97]) - log(h[0]));
+}
+
+void CasTest::error_export()
+{
+    ofstream erreur_schema_max;
+	ofstream erreur_schema_L2;
+    string nom_erreur_max = nom_schema + "_erreur_schema_max.txt";
+	string nom_erreur_L2 = nom_schema + "_erreur_schema_L2.txt";
+    erreur_schema_max.open(nom_erreur_max);
+	erreur_schema_L2.open(nom_erreur_L2);
+    calcul_erreur_totale();
+    cout << calcul_pente_max() << endl;
+    for (unsigned long long i = 2; i < (N_max_erreurs - N_min_erreurs) / (int)pas_erreurs; i++)
+    {
+        erreur_schema_max << h[i] << " " << erreur_max[i] << " " << log(erreur_max[i]) << endl;
+		erreur_schema_L2 << h[i] << " " << erreur_L2[i] << " " << log(erreur_L2[i]) << endl;
+    }
+    erreur_schema_max.close();
+}
+
+// void CasTest::exact_export(double n){
 //	string nom_solution_exate = nom_schema + "_solution_exacte.txt";
 //	ofstream solution_exacte;
 //    solution_exacte.open(nom_solution_exate);
@@ -106,14 +112,12 @@ void CasTest::error_export(){
 //    solution_exacte.close();
 //}
 
-
-
-//PbCauchy CasTest::set_pbCauchy(){
+// PbCauchy CasTest::set_pbCauchy(){
 //	PbCauchy EDO_test = PbCauchy( fct_sol_exacte(a), fct_second_membre);
 //	return EDO_test;
 //}
 //
-//Solver CasTest::set_schema(double n){
+// Solver CasTest::set_schema(double n){
 //	// On voit le type se schema voulu
 //	if(type_schema == "EuExp"){
 //		Solver schema = EulerExplicite(a, b, n, nom_schema, set_pbCauchy());
@@ -127,7 +131,7 @@ void CasTest::error_export(){
 //}
 
 //
-//void CasTest::maj_schema(double n){
+// void CasTest::maj_schema(double n){
 //	N = n;
 //	calcul();
 //	for(double i =0; i<n; i++){
@@ -135,7 +139,7 @@ void CasTest::error_export(){
 //	}
 //}
 //
-//void CasTest::tps(double n){
+// void CasTest::tps(double n){
 //	temps.clear();
 //	temps.resize(n);
 //	temps.push_back(a);
@@ -149,7 +153,7 @@ void CasTest::error_export(){
 //	temps.push_back(b);
 //}
 //
-//void CasTest::exacte(double n){
+// void CasTest::exacte(double n){
 ////	sol_exacte.resize(n);
 //	N = n;
 //	tps(n);
@@ -160,7 +164,7 @@ void CasTest::error_export(){
 //    }
 //}
 //
-//void CasTest::approx(double n){
+// void CasTest::approx(double n){
 ////	sol_approch.resize(n);
 //	maj_schema(n);
 ////	sol_approch = schema.x_val;
@@ -170,7 +174,7 @@ void CasTest::error_export(){
 //	}
 //}
 //
-//void CasTest::erreur(double n){
+// void CasTest::erreur(double n){
 ////	Solver schema = set_schema(n);
 ////	maj_vects(n, schema);
 //	maj_schema(n);
@@ -183,7 +187,7 @@ void CasTest::error_export(){
 //    error_tot.push_back(sqrt(err));
 //}
 //
-//void CasTest::calcul_error(){
+// void CasTest::calcul_error(){
 //    for(double n= N_min; n<= N_max; n+=pas){
 //        maj_schema(n);
 //        erreur(n);
@@ -193,7 +197,7 @@ void CasTest::error_export(){
 //
 
 //
-//void CasTest::exact_export(double n){
+// void CasTest::exact_export(double n){
 //	string nom_solution_exate = nom_schema + "_solution_exacte.txt";
 //	ofstream solution_exacte;
 //    solution_exacte.open(nom_solution_exate);
@@ -205,7 +209,7 @@ void CasTest::error_export(){
 //    solution_exacte.close();
 //}
 //
-//void CasTest::error_export(){
+// void CasTest::error_export(){
 //    ofstream erreur_schema;
 //	string nom_erreur = nom_schema + "_erreur_schema.txt";
 //    erreur_schema.open(nom_erreur);
