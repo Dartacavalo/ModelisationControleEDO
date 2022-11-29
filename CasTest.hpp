c4d5cb079eb1c848ab7fab48b3f95445a65cf19b
themathsdude <elyass2008@hotmail.fr>
2022-11-29 16:16:51 +0100

    variable order change for -Wall flag

--- CasTest.hpp
+++ CasTest.hpp
@@ -17,27 +17,29 @@ class CasTest
   public:
     // les dépendances évidentes
     PbCauchy pbcauchy; // Le pb de Cauchy dont solution exacte connue est a tester;
-
-    // les nouveaux objets à construire
     function<double(double, double, double)> fct_sol_exacte; // La solution exacte du probleme de Cauchy
-    vector<double> erreur_max;               // Vecteur qui stocke le calcul de l'erreur totale en norme infinie
-    vector<double> erreur_L2;                // Vecteur qui stocke le calcul de l'erreur totale en norme infinie
-    vector<double> h;                        // Les abscisses
-    vector<double> vec_sol_exacte;
+
     // les paramètres de test
-    double const a, b;                         // l'intervalle du test
-    double const N_min_erreurs, N_max_erreurs; // Le range de nos tests
-    double const pas_erreurs;                  // Le pas
+    double a, b;                         // l'intervalle du test
+    const unsigned long long N_min_erreurs, N_max_erreurs; // Le range de nos tests
+    const unsigned long long pas_erreurs;                  // Le pas
     string nom_schema;
     string type_schema;
+
+    // les nouveaux objets à construire
+    vector<double> erreur_max; // Vecteur qui stocke le calcul de l'erreur totale en norme infinie
+    vector<double> erreur_L2;  // Vecteur qui stocke le calcul de l'erreur totale en norme infinie
+    vector<double> h;          // Les abscisses
+    vector<double> vec_sol_exacte;
 
     // Arguments intermediaires qui changeront de taille avec chaque nouveau calcul d erreur
     // vector<double> tps;
     // vector<double> sol_approch;
 
     // Le constructeur de CasTest
-    CasTest(PbCauchy _pbcauchy, function<double(double, double, double)> _fct_sol_exacte, const double _a, const double _b,
-            double const _N_min, double const _N_max, double const _pas, string _nom_schema, string _type_schema)
+    CasTest(PbCauchy _pbcauchy, function<double(double, double, double)> _fct_sol_exacte, const double _a,
+            const double _b, double const _N_min, double const _N_max, double const _pas, string _nom_schema,
+            string _type_schema)
         : pbcauchy(_pbcauchy), fct_sol_exacte(_fct_sol_exacte), a(_a), b(_b), N_min_erreurs(_N_min),
           N_max_erreurs(_N_max), pas_erreurs(_pas), nom_schema(_nom_schema), type_schema(_type_schema){};
 
@@ -52,7 +54,6 @@ class CasTest
 
     void expor();
     void error_export();
-
 
     //    // L'exportation de la solution exacte pour superposer à la solution approchee
     //    void exact_export();
